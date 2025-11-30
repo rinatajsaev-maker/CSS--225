304d3b3459d5413166f96d9af2d6af07fd2e694e
rinatajsaev-maker <rinatajsaev@gmail.com>
2025-11-29 22:37:18 -0600

    Add files via upload

---
+++ week 4/time (3).py
@@ -0,0 +1,9 @@
+currentTimeStr = input("What is the current time (in hours 0-23)?")
+waitTimeStr = input("How many hours do you want to wait")
+
+currentTimeInt = int(currentTimeStr)
+waitTimeInt = int(waitTimestr)
+
+finalTimeInt = currentTimeInt + waitTimeInt
+print(finalTimeInt)
+
